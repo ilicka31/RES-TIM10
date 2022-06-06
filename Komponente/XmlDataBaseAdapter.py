f444164afdbfbf2d8e17d95c153d370b5efca764
sloviclana <slovicl17@gmail.com>
2022-06-06 18:01:36 +0200

    XmlDataBase adapter ispravka

--- Komponente/XmlDataBaseAdapter.py
+++ Komponente/XmlDataBaseAdapter.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import xml.etree.ElementTree as ET
 from Komponente.repozitorijum import connection
 import mysql.connector
@@ -5,8 +6,8 @@ import mysql.connector
 #ovde treba da imam vezu sa bazom i jedan fajl u kom cu
 #privremeno da cuvam xml parsiran iz JSON-a
 
-def ToSql():
-    tree = ET.parse('Temp.xml')
+def ToSql(data):
+    tree = ET.parse(data)   #parsira direkt iz prethodnog adaptera
     data2 = tree.findall('request')
 
     sqlZahtev = ""
@@ -14,8 +15,16 @@ def ToSql():
     for i, j in zip(data2, range(1, 4)):
         verb = i.find('verb').text
         noun = i.find('noun').text
-        query = i.find('query').text
-        fields = i.find('fields').text
+        
+        if(i.find('query') != NULL):
+            query = i.find('query').text
+        else:
+            query = ""
+
+        if(i.find('fields') != NULL):
+            fields = i.find('fields').text
+        else:
+            fields = ""
 
         
         if(verb == 'GET'):
@@ -32,31 +41,16 @@ def ToSql():
 
     return sqlZahtev
 
-def PosaljiZahtev(sqlZahtev):
-    odgovor = ""
-
-    #salje sql na repozitorijum i preuzima odgovor iz repozitorijuma
-    return odgovor
-
-def BackToXml(sqlOdgovor):
+def BackToXml(sqlZahtev):
     
     #preuzmi odgovor iz baze (kog je formata?) i dobavi vrednosti ovih polja:
+    #koneektuje se na repo, posalje sqlZahtev koji je prosledjen i preuzme odgovor koji se dalje parsira
 
     status = ""
     status_code = ""
     payload = ""
-    xmlOdgovor = "<status>" + status + "</status> <status_code>" + status_code + "</status_code> <payload>"+payload+"</payload>";
+    xmlOdgovor = "<response><status>" + status + "</status> <status_code>" + status_code + "</status_code> <payload>"+payload+"</payload></response>";
 
     return xmlOdgovor
 
 
-def XmlAdapter():
-
-    sqlRequest = ToSql()
-
-    #konekcija sa repo u metodi posalji zahtev
-    odgovor = PosaljiZahtev(sqlRequest)
-
-    xmlOdgovor = BackToXml(odgovor)
-
-    return xmlOdgovor
