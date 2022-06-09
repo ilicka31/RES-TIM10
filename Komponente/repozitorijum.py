b2a8228ea3d8818711fa3341e33523d73317b106
vladimir302019 <vladimir9stanojevic@gmail.com>
2022-06-09 12:34:11 +0200

    Omogucena TCP konekcija sa XmlDataBaseAdapter

--- Komponente/repozitorijum.py
+++ Komponente/repozitorijum.py
@@ -4,11 +4,16 @@ from mysql.connector import Error
 from setuptools import find_namespace_packages
 #from Komponente.XmlDataBaseAdapter import *
 
+import socket
+import random
+import time
+
+
 try:
     connection = mysql.connector.connect(host='localhost',
                                         database='repozitorijum',
                                         user='root',
-                                        password='root')
+                                        password='vlada991')
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server verison ", db_Info)
@@ -25,3 +30,17 @@ except Error as e:
 #        cursor.close()
 #        connection.close()
 #        print("MySQL connection is closed")
+
+
+####KONEKCIJA SA XMLADAPTEROM
+TCP_IP = '127.0.0.1'
+TCP_PORT2 = 8007
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT2))
+
+sqlzahtev = s.recv(BUFFER_SIZE)
+print(sqlzahtev)
+#cursor.execute(sqlzahtev)
+
+s.close()
